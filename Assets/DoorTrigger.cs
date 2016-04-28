d197368bd545804717cdc41ecb14194e618dbfd2
Martin Leissler <martin@leissler.net>
2016-04-28 22:46:44 +0200

    Use C# Delegate/Event mechanism instead of UnityEvents

--- Assets/DoorTrigger.cs
+++ Assets/DoorTrigger.cs
@@ -1,30 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.Events;
+//using UnityEngine.Events;
 
 
 [RequireComponent (typeof (Collider))]
 
 public class DoorTrigger : MonoBehaviour {
-	
-	public UnityEvent OnExit;
-	public UnityEvent OnEnter;
+
+	public delegate void OnExitEvent();
+	public event OnExitEvent OnExit;
+
+	public delegate void OnEnterEvent();
+	public event OnEnterEvent OnEnter;
 
 	void Start() {
 		Openable[] openableObjects = Object.FindObjectsOfType<Openable>();
 		foreach (Openable o in openableObjects) {
-			OnEnter.AddListener(o.Open);
-			OnExit.AddListener(o.Close);
+			OnEnter += o.Open;
+			OnExit += o.Close;
 		}
 
 	}
 
 	void OnTriggerExit(Collider other) {
-		OnExit.Invoke();
+		OnExit();
 	}
 
 	void OnTriggerEnter(Collider other) {
-		OnEnter.Invoke();
+		OnEnter();
 	}
 		
 }
